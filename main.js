d1ff48c55351c2f27f15bcc6eb8e4fbb0baf3d60
권도0 <i77421563@gmail.com>
2026-03-24 15:40:50 +0900

    main

--- main.js
+++ main.js
@@ -3,35 +3,36 @@ document.getElementById('time').innerHTML = new Date().toLocaleString()
 function pink() {
   document.body.style.backgroundColor = 'pink'
 }
-function yello() {
-  document.body.style.backgroundColor = 'yello'
-}
+
 function lightblue() {
   document.body.style.backgroundColor = 'lightblue'
 }
+function yellow() {
+  document.body.style.backgroundColor = 'yellow'
+}
 function reset() {
-  document.body.style.backgroundColor = 'pink'
+  document.body.style.backgroundColor = 'linen'
 }
 
 function showhtml() {
-  document.getElementById('fig').sry = 'img/html5.png'
+  document.getElementById('fig').src = 'img/html5.png'
   document.getElementById('desc').innerHTML =
-    'HTML은 웹문서를 작성하는데 사용하는 마크업 언어이다'
+    '<h3>HTML</h3>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
 }
 
 function showcss() {
-  document.getElementById('fig').sry = 'img/css3.png'
+  document.getElementById('fig').src = 'img/css3.png'
   document.getElementById('desc').innerHTML =
-    'css은 웹문서의 스타일을 지정하는데 사용하는 언어이다'
+    '<span style="color:red;">CSS3</span>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
 }
 
 function showjs() {
-  document.getElementById('fig').sry = 'img/javascript.png'
+  document.getElementById('fig').src = 'img/javascript.png'
   document.getElementById('desc').innerHTML =
-    'javascript는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다.'
+    'Javascript는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다.'
 }
 
 function hide() {
-  document.getElementById('fig').sry = ''
+  document.getElementById('fig').src = ''
   document.getElementById('desc').innerHTML = ''
 }
